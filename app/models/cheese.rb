76cbf2d72e81916129bedaf18c825ac1696a10e9
Paul Jung <pbnjcub@gmail.com>
2023-03-22 09:14:16 -0400

    lab done

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,5 +1,19 @@
 class Cheese < ApplicationRecord
   
+  def index 
+    cheeses = Cheese.all
+    render json: cheeses, except: [:created_at, :updated_at]
+  end
+
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { message: 'Cheese not found' }, status: :not_found
+    end
+  end
+
   def summary
     "#{name}: $#{price}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
   get '/cheeses/:id', to: 'cheeses#show'
+  get '/hello_cheeses', to: 'cheeses#hello_cheeses'
 end
